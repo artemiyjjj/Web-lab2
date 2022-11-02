95fb3f6d95c22cb689cc4039f2fa541865a6d45f
artemioJjJ <artemiyromanoff@gmail.com>
2022-11-02 11:31:02 +0300

    Added exceptions

---
+++ src/main/java/com/backend/lab2vol4/exeptions/FormDataValidationException.java
@@ -0,0 +1,7 @@
+package com.backend.lab2vol4.exeptions;
+
+public class FormDataValidationException extends Exception {
+    public FormDataValidationException(String message) {
+        super(message);
+    }
+}
